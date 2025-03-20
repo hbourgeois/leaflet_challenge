0cf55b153524820e91b3a6498146273379451611
hbourgeois <h.bourgeoispdx@gmail.com>
2025-03-20 08:58:23 -0700

    tested js and css files, confirmed citations and comments are correct

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # leaflet_challenge
+
+#### Background/Overview:
+
+The United States Geological Survey (USGS) is dedicated to providing scientific data on natural hazards, ecosystem and environmental health, and the effects of climate and land-use changes. Their scientists innovate new methods and tools to deliver timely, relevant, and useful information about Earth's processes.
+
+USGS aims to develop new tools for visualizing their earthquake data. Despite collecting vast amounts of data globally each day, they currently lack an effective way to display it. This project seeks to create a visualization system for USGS data, enhancing public education and informing other government organizations about planetary issues, potentially securing more funding in the process.
+
+
+
+---
+
+
+
+#### **References:**
+
+**Data:**
+
+* Source: *Geojson summary format* . U.S. Geological Survey. (n.d.). https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+* JSON representation: US Geological Survey. (n.d.).  *GeoJSON Data Representation - All Earthquakes from the Past 7 Days* . Earthquake Hazards Program. https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
+
+**Leaflet tutorials:**
+
+* Using GeoJSON with Leaflet - https://leafletjs.com/examples/geojson/
+  Layer Groups and Layers Control - https://leafletjs.com/examples/layers-control/
+
+**CSS fonts / colors:**
+
+* https://www.w3schools.com/css/css_font.asp
+  https://stackoverflow.com/questions/42925682/where-to-set-font-family-body-or-html-element
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,85 +1,106 @@
-// Create the 'basemap' tile layer that will be the background of our map.
 
 // establish geoJSON endpoint
 // geojson data for the past week
 let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+    
+  // Once we get a response, send the data.features object to the createFeatures function.
+  createFeatures(data.features);
+});
 
-// Create the map object with center and zoom options.
+// Create feature layers for the earthquake data points
+function createFeatures(earthquakeData) {
+    // Define a function that runs for each feature in the features array
+    function onEachFeature(feature, layer) {
+      layer.bindPopup(`<h3>${feature.properties.place}
+          <hr>
+          <p>Magnitude: ${feature.properties.mag}</p>
+          <p>Depth: ${feature.geometry.coordinates[2]}km</p>
+          <p>${new Date(feature.properties.time)}`);
+   }
 
-
-// Then add the 'basemap' tile layer to the map.
-
-// OPTIONAL: Step 2
-// Create the layer groups, base maps, and overlays for our two sets of data, earthquakes and tectonic_plates.
-// Add a control to the map that will allow the user to change which layers are visible.
-
-
-// Make a request that retrieves the earthquake geoJSON data.
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
-
-  // This function returns the style data for each of the earthquakes we plot on
-  // the map. Pass the magnitude and depth of the earthquake into two separate functions
-  // to calculate the color and radius.
-  function styleInfo(feature) {
-
-  }
-
-  // This function determines the color of the marker based on the depth of the earthquake.
-  function getColor(depth) {
-
-  }
-
-  // This function determines the radius of the earthquake marker based on its magnitude.
-  function getRadius(magnitude) {
-
-  }
-
-  // Add a GeoJSON layer to the map once the file is loaded.
-  L.geoJson(data, {
-    // Turn each feature into a circleMarker on the map.
-    pointToLayer: function (feature, latlng) {
-
-    },
-    // Set the style for each circleMarker using our styleInfo function.
-    style: styleInfo,
-    // Create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
-    onEachFeature: function (feature, layer) {
-
+    function getColor(depth) {
+      return depth > 80 ? 'rgb(145, 6, 6)' :
+             depth > 60 ? 'rgb(247, 42, 42)' :
+             depth > 40 ? 'rgb(236, 138, 47)' :
+             depth > 20 ? 'rgba(236, 233, 49, 0.9)' :
+                         'rgba(102, 255, 0, 0.67)';
     }
-  // OPTIONAL: Step 2
-  // Add the data to the earthquake layer instead of directly to the map.
-  }).addTo(map);
 
-  // Create a legend control object.
-  let legend = L.control({
-    position: "bottomright"
+    // Create a GeoJSON layer that contains the features array
+    let earthquakes = L.geoJSON(earthquakeData, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, {
+                radius: feature.properties.mag * 5, // radius of the circle marker
+                fillColor: getColor(feature.geometry.coordinates[2]),
+                color: '#000',
+                weight: 0.8,
+                opacity: 0.3,
+                fillOpacity: 0.8
+            });
+        },
+        onEachFeature: onEachFeature
+    });
+
+    // Create the map layer for the earthquake data
+    createMap(earthquakes);
+}
+
+// Create the map object
+function createMap(earthquakes) {
+  // add the tile layers.
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  })
+
+  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
   });
 
-  // Then add all the details for the legend
-  legend.onAdd = function () {
-    let div = L.DomUtil.create("div", "info legend");
-
-    // Initialize depth intervals and colors for the legend
-
-
-    // Loop through our depth intervals to generate a label with a colored square for each interval.
-
-
-    return div;
+  // Create the baseMaps object.
+  let baseMaps = {
+    "Street": street,
+    "Topographical": topo
   };
 
-  // Finally, add the legend to the map.
+  // Create an overlay object to hold our overlay.
+  let overlayMaps = {
+    Earthquakes: earthquakes
+  };
 
-
-  // OPTIONAL: Step 2
-  // Make a request to get our Tectonic Plate geoJSON data.
-  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function (plate_data) {
-    // Save the geoJSON data, along with style information, to the tectonic_plates layer.
-
-
-    // Then add the tectonic_plates layer to the map.
-
+  // Create our map, giving it the streetmap and earthquakes layers to display on load.
+  let myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [street, earthquakes]
   });
-});
+
+  // Create a layer control and add to the map.
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+  // Set up the legend.
+  let legend = L.control({ position: "bottomright" });
+
+  legend.onAdd = function() {
+    // create div
+    let div = L.DomUtil.create('div', 'legend');
+    // add legend content
+    div.innerHTML = "<h2><center>Legend</center></h2>" +
+                  "<i style='background:rgb(145, 6, 6)'></i> depth > 80 km<br>" +
+                  "<i style='background:rgb(247, 42, 42)'></i> 60 - 80 km<br>" +
+                  "<i style='background:rgb(236, 138, 47)'></i> 40 - 60 km<br>" +
+                  "<i style='background:rgba(236, 233, 49, 0.9)'></i> 20 - 40 km<br>" +
+                  "<i style='background:rgba(102, 255, 0, 0.67)'></i> O - 20 km<br>";
+  
+  return div;
+  };
+  
+  legend.addTo(myMap);
+}
